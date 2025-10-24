7008bf4e18d69fc1c0292b757246e3d78ae12d29
Starflight-code <sparkr123@gmail.com>
2025-10-24 02:05:29 -0500

    Add: Security Page Added a security page, fixed soem issues on other pages closes #12

---
+++ web/security.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", async () => {
+  let contact_col = document.getElementById("contact-alerts-col");
+  let contacts = await fetch("./static/contacts.json");
+  let contacts_json = await contacts.json();
+  console.log(contacts_json);
+
+  for (let contact of contacts_json) {
+    for (let alert of contact["alerts"]) {
+      let div = document.createElement("div");
+      div.classList.add("card");
+      div.classList.add("mb-2");
+      div.innerHTML = `
+        <div class="row g-0">
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${alert["type"]}</h5>
+              <p class="card-text">
+                From: ${contact["firstName"]} ${contact["lastName"]}<br />Severity: ${alert["severity"]}
+              </p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <img src="${contact['image']}" class="img-fluid rounded-start" style="max-height: 150px" alt="${contact["firstName"]} ${contact["lastName"]}">
+          </div>`
+      contact_col.appendChild(div);
+    }
+  }
+})
