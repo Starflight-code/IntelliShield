588b165556ac70dfbf2750ee41a96f4dbeae7dce
Starflight-code <sparkr123@gmail.com>
2025-12-08 00:16:07 -0600

    Add: mongodb, Change: import formatting

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const fs = require('fs');
+import { express } from 'express'
+import { cors } from 'cors'
+import { path } from 'path'
+import { fs } from 'fs'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
